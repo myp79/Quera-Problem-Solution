3665a575f7aae386e282a67c4efb9ffc75681abf
Mohammad YousefiPour <moyousefipour79@gmail.com>
2021-12-27 03:25:54 +0330

    problem 2885
    solving the problem 2885 with C++.

---
+++ Competition/2885/solve_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "man khoshghlab hastam" << endl;
+    }
+
+    return 0;
+}
+// Mohammad YousefiPour - Github.com/myp79
