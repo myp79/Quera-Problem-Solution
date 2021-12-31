2fd2661ff045dfded4abd8c5109fc86398539ece
Mohammad YousefiPour <moyousefipour79@gmail.com>
2022-01-01 01:03:06 +0330

    problem 2636
    solving the problem 2636 with C++.

---
+++ Competition/2636/solve_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m[6];
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> m[i];
+    }
+    cout << 1 - m[0] << " " << 1 - m[1] << " " << 2 - m[2] << " " << 2 - m[3] << " " << 2 - m[4] << " " << 8 - m[5];
+    return 0;
+}
+// Mohammad YousefiPour - Github.com/myp79
