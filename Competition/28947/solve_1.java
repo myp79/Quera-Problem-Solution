e8b4a7f9c8a8631f8b8f782c92a65cb36e870dce
Mohammad YousefiPour <moyousefipour79@gmail.com>
2022-01-02 02:58:27 +0330

    problem 28947
    solving the problem 28947 with Java.

---
+++ Competition/28947/solve_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class solve_1 {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String[] sin = {
+      "sib",
+      "samanoo",
+      "sabze",
+      "senjed",
+      "serke",
+      "seke",
+      "sonbol",
+    };
+    int x = input.nextInt();
+    for (int i = 0; i < x; i++) {
+      System.out.println(sin[i]);
+    }
+  }
+}
+/**
+ * @author Mohammad YousefiPour - Github.com/myp79
+ */
