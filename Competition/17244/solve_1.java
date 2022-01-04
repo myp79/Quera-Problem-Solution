e5b95bf5cbf4d1ec1965f95fccccca4bfab81ab4
Mohammad YousefiPour <moyousefipour79@gmail.com>
2022-01-04 05:08:19 +0330

    problem 17244
    solving the problem 17244 with Java.

---
+++ Competition/17244/solve_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class solve_1 {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int k = input.nextInt();
+    int sum = 0;
+    for (int i = 0; i < k; i++) {
+      sum += i + 1;
+    }
+    System.out.println(sum);
+  }
+}
+/**
+ * @author Mohammad YousefiPour - Github.cosdm/myp79
+ */
