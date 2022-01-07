578190f720abeee900a2239f30caad900a92795b
Mohammad YousefiPour <moyousefipour79@gmail.com>
2022-01-07 10:39:19 +0330

    problem 2794
    solving the problem 2794 with C++.

---
+++ Competition/2794/solve_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d,e,f;
+    cin >> a>>b>>c>>d>>e>>f;
+    if(d!=f){
+        cout<<c+e-a<<" "<<f+d-b;
+    }
+    else{
+        cout<<a+c-e<<" "<<+b+d-f;
+    }
+    return 0;
+}
+// Mohammad YousefiPour - Github.com/myp79
