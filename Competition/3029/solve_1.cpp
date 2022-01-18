59355ed9bc75ccba89b8c49970969c8b29294cc2
Mohammad YousefiPour <moyousefipour79@gmail.com>
2022-01-18 12:32:22 +0330

    problem 3029
    solving the problem 3029 with C++.

---
+++ Competition/3029/solve_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int a,b,c,d;
+   cin>>a>>b>>c>>d;
+   if(c<a){
+       cout<<"Left";
+   }
+   else{
+       cout<<"Right";
+   }
+    return 0;
+}
+// Mohammad YousefiPour - Github.com/myp79
