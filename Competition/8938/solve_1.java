c49bda3dc8d74075b49714e87592b36a50f0849f
Mohammad YousefiPour <moyousefipour79@gmail.com>
2022-01-24 12:49:53 +0330

    problem 8938
    solving the problem 8938 with Java.

---
+++ Competition/8938/solve_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class solve_1 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int m = scanner.nextInt();
+    int n = scanner.nextInt();
+    int sum = 0;
+    int[][] matrix = new int[m][m];
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < m; j++) {
+        matrix[i][j] = scanner.nextInt();
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      int x = scanner.nextInt();
+      int y = scanner.nextInt();
+      sum += matrix[x - 1][y - 1];
+    }
+    System.out.print(sum);
+  }
+}
+/**
+ * @author Mohammad YousefiPour - Github.com/myp79
+ */
