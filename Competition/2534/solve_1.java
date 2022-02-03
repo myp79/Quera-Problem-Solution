baf40d58948f5828f7c63b8a71ca209a146bdd23
Mohammad YousefiPour <moyousefipour79@gmail.com>
2022-02-03 12:46:27 +0330

    problem 2534
    solving the problem 2534 with Java.

---
+++ Competition/2534/solve_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class solve_1 {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        int sum = 0, sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
+        }
+        sum = sum / n;
+        for (int i = 0; i < n; i++) {
+            int a = arr[i] - sum;
+            if (a > 0) {
+                sum2 += a;
+            }
+        }
+        System.out.println(sum2);
+    }
+}
